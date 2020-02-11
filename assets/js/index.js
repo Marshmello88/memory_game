97d2d553b7a8213c34700a8c74fad37d19beade8
Marshmello88 <m.neibauere@gmail.com>
2020-02-11 19:58:22 +0000

    changes to matching function and timer added

--- assets/js/index.js
+++ assets/js/index.js
@@ -62,7 +62,7 @@ const gameGrid = cardsArray
 const memorygame = document.getElementById('memorygame')
 const gameBoard = document.createElement('section')
 gameBoard.setAttribute('class', 'gameBoard')
-gameBoard.setAttribute('onclick', 'checkForMatch(this);');
+// gameBoard.setAttribute('onclick', 'checkForMatch(this);');
 
 // Append the section to the memorygame div
 memorygame.appendChild(gameBoard);
@@ -95,16 +95,31 @@ const cards = document.querySelectorAll('.card');
 // loop through them with forEach and attach event listener 
 //The first parameter is the type of the event (like "click" or "mousedown" or any other HTML DOM Event.)
 // The second parameter is the function we want to call when the event occurs
-cards.forEach(card => card.addEventListener('click', displayCard));
-cards.forEach(card => card.addEventListener('click', checkForMatch));
-//cards.forEach(card => card.addEventListener('click', lockBoard));
+
+// the "event" object is always passed to the the event handler function
+cards.forEach(card => card.addEventListener('click', cardIsClicked));
+
+function cardIsClicked(event){
+    // access the event target (the element that got clicked)
+    let card=event.currentTarget; 
+    if(openedCards.length == 0){
+        displayCard(card);
+        openedCards.push(card)
+    } else if (openedCards.length == 1) {
+        openedCards.push(card)
+        displayCard(card);
+        checkForMatch()
+    } else {
+        return;
+    }
+}
 
 
 
 // flipCard function gets triggered upon 
-function displayCard() {
-     if (isProcessing) { return; }
-    this.classList.add('display'); //display class gets toggled, refer to css
+function displayCard(card) {
+    //  if (isProcessing) { return; }
+    card.classList.add('display'); //display class gets toggled, refer to css
 }
 
 // add the card to a *list* of "open" cards 
@@ -115,15 +130,14 @@ let count = 0;
 let isProcessing = false;
 
 function checkForMatch() {
-    openedCards.push(this);
-     console.log("openedCards = " + openedCards)
-    //console.log("openedCards[0].innerHTML = " + openedCards[0].innerHTML)
-    //console.log("openedCards[2].innerHTML = " + openedCards[2].innerHTML)
-    if (openedCards.length > 2 && openedCards[0].innerHTML === openedCards[2].innerHTML) {
-        //(openedCards[0].isEqualNode(openedCards[2])) {
-            isProcessing = true;
+    let cardValue0 = openedCards[0].getAttribute("data-name");
+    let cardValue1 = openedCards[1].getAttribute("data-name")
+
+    if (cardValue0 === cardValue1) {
         disableCards();
-        return;
+        //save the guessed cards
+         matchedCards.push(cardValue0, cardValue1)
+        console.log(matchedCards);
     } else {
         unflipCards();
         isProcessing = false;
@@ -134,7 +148,7 @@ function checkForMatch() {
 function disableCards() {
     console.log(disableCards);
     openedCards[0].removeEventListener('click', displayCard);
-    openedCards[2].removeEventListener('click', displayCard);
+    openedCards[1].removeEventListener('click', displayCard);
     openedCards = []; //this is not emptying the array, why?
 }
 
@@ -143,16 +157,57 @@ function unflipCards() {
     console.log(unflipCards);
     setTimeout(() => {
         openedCards[0].classList.remove('display');
-        openedCards[2].classList.remove('display');
+        openedCards[1].classList.remove('display');
         openedCards = []; //empty the array
-    }, 1100);
+    }, 2000);
 }
 
-//debugger;
+debugger;
 
-function lockBoard(){
+/*function lockBoard(){
     console.log(lockBoard);
     if (openedCards.length === 2) {
        cardsArray[i].classList.add('disabled')};
+}*/
+
+//you win
+ function winGame(){
+     if (matchedCards.length === 26) {
+
+     }
+ }
+
+ // game timer
+ //setInterval(function, milliseconds). Executes a function after milliseconds, but repeats the execution of the function continuously.
+
+//https://codepad.co/snippet/javascript-stopwatch-using-javascript-and-css
+var seconds = 0, minutes = 0; hours = 0;
+var timer = document.querySelector(".timer");
+
+function startTimer() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+timer.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+ //timer.innerHTML = minutes + "minutes " + seconds + "seconds";
+    clock();
 }
+
+function clock() {
+    t = setTimeout(startTimer, 1000);
+}
+clock();
+
+
+
+
+
+
 
